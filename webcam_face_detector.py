b21285382358bb022e02914e5b7777e21496161d
hairav <keshav.kumar.cse16@itbhu.ac.in>
2017-12-15 16:26:09 +0530

    drowsiness detector added

--- webcam_face_detector.py
+++ webcam_face_detector.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-cap = cv2.VideoCapture(1)
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
